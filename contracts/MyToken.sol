127da1930204758c32e180d0a8a98088f989e096
doyeon2004 <dy2004kim@gmail.com>
2025-07-04 22:32:53 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,20 +9,21 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimal, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-
-        // transaction
-        // from, to, data, value, gas, ...
         _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT (e.g. 10^18 for 18 decimals)
     }
 
     function _mint(uint256 amount, address owner) internal {
-        // totalSupply = totalSupply + amount;
-        // balanceOf[owner] = balanceOf[owner] + amount;
         totalSupply += amount;
         balanceOf[owner] += amount;
+    }
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
     }
 }
